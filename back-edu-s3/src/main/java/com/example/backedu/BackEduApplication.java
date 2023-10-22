805aeaf2859763c42911d80f80c19e6cc4c9d774
KangTaeGyung <96178104+KangTaeGyung@users.noreply.github.com>
2023-10-22 19:20:42 +0900

    new commit

---
+++ back-edu-s3/src/main/java/com/example/backedu/BackEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.backedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEduApplication.class, args);
+    }
+
+}
